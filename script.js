376fc21c061ffc1f459339c7d8ac7561909352a2
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-06-23 18:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,26 +4,3 @@
             myModal.show();
         }
 
-() => {
-  cy.get('h2').should('have.text', 'Cards Gallery');
-  cy.get('.container .row').should('have.length', 1);
-}
-
-() => {
-  const tags = ['img', 'h6', 'p'];
-  tags.forEach(tag => {
-    cy.get(tag).should('have.length', 6); // expect exactly 6 of each
-  });
-}
-
-cy.get('.card img').should('have.length', 6);
-cy.get('.card h6').should('have.length', 6);
-cy.get('.card p').should('have.length', 6);
-
-
-cy.get('.card').should('have.length.at.least', 6).within(() => {
-  cy.get('.card-body').should('exist');
-});
-cy.get('.lightbox').should('have.length.at.least', 6);
-
-
