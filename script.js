a588238f679153e3f50ecad5f240fe6acb1ad0ab
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-06-23 23:38:33 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
- function openModal(imageSrc) {
-            document.getElementById('modalImage').src = imageSrc;
-            var myModal = new bootstrap.Modal(document.getElementById('imageModal'));
-            myModal.show();
-        }
-
-
-
